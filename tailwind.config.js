7664dec3ef02d9f0e30c2a744546a9908ba72a5d
iheb147 <iheb.zeddini@esprit.tn>
2025-02-27 03:41:30 -1200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary :"#9a074c",
+        secondary:"#ff3946",
+      },
+    },
+  },
+  plugins: [],
+}
+
