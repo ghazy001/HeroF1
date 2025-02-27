4de1500d847aa1968ee31e226c290c7326c13f6e
Israa23 <israa.boulaares@esprit.tn>
2025-02-27 16:41:52 +0100

    HomePage

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E10500",
+        secondary: "#F5F5F5",
+      },
+    },
+  },
+  plugins: [],
+};
